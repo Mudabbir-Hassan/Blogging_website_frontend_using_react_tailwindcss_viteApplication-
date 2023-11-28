94dca8a116dcb0a98f7112d5ffa6ad1870ce8ddd
Mudabbir-Hassan <mudabbirdar5@gmail.com>
2023-11-28 11:35:39 +0500

    nav bar responsiveness added

--- ghost_blog/src/pages/Navbar/index.jsx
+++ ghost_blog/src/pages/Navbar/index.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
 const NavBar = () => {
+  const [menuVisible, setMenuVisible] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuVisible(!menuVisible);
+  };
+
   return (
     <>
       <nav className="bg-black text-white p-4 flex flex-col md:flex-row justify-between items-center">
@@ -18,7 +24,10 @@ const NavBar = () => {
         </NavLink>
 
         {/* Menu Icon (Visible on small screens) */}
-        <button className="md:hidden text-white">
+        <button
+          className="md:hidden text-white"
+          onClick={toggleMenu}
+        >
           <svg
             className="w-6 h-6"
             fill="none"
@@ -36,32 +45,32 @@ const NavBar = () => {
         </button>
 
         {/* Menu Items (Hidden on small screens) */}
-        <ul className="hidden md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4">
+        <ul className={`md:flex flex-col md:flex-row space-y-2 md:space-y-0 md:space-x-4 ${menuVisible ? 'block' : 'hidden'}`}>
           <li className="hover:text-gray-300 transition duration-300">
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="/" onClick={toggleMenu}>Home</NavLink>
           </li>
           <li className="hover:text-gray-300 transition duration-300">
-            <NavLink to="/service">Services</NavLink>
+            <NavLink to="/service" onClick={toggleMenu}>Services</NavLink>
           </li>
           <li className="hover:text-gray-300 transition duration-300">
-            <NavLink to="/about">About</NavLink>
+            <NavLink to="/about" onClick={toggleMenu}>About</NavLink>
           </li>
           <li className="hover:text-gray-300 transition duration-300">
-            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/blog" onClick={toggleMenu}>Blog</NavLink>
           </li>
           <li className="hover:text-gray-300 transition duration-300">
-            <NavLink to="/contact-us">Contact Us</NavLink>
+            <NavLink to="/contact-us" onClick={toggleMenu}>Contact Us</NavLink>
           </li>
         </ul>
 
         {/* Login/Logout (Hidden on small screens) */}
-        <div className="mt-4 md:mt-0 hidden md:block">
+        <div className={`mt-4 md:mt-0 hidden md:block ${menuVisible ? 'block' : 'hidden'}`}>
           <span className="hover:text-blue-300 transition duration-300">
-            <NavLink to="log-in">Login</NavLink>
+            <NavLink to="log-in" onClick={toggleMenu}>Login</NavLink>
           </span>
           <span className="mx-2">|</span>
           <span className="hover:text-red-500 transition duration-300">
-            <NavLink to="/">Logout</NavLink>
+            <NavLink to="/" onClick={toggleMenu}>Logout</NavLink>
           </span>
         </div>
       </nav>
